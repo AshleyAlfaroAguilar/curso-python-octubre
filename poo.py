6d638b204ff9a29ad2eef7a3b72f9c4a221854a4
Ashley Alfaro <alfaroashley25@outlook.es>
2022-12-12 19:30:25 -0600

    add exercises about poo properties

--- poo.py
+++ poo.py
@@ -27,3 +27,81 @@ billy = Member('Billy')
 #billy.name = 'Billy'
 billy.welcome()
 
+#herencia
+class Person():
+    first_name= None
+    last_name = None
+    birthdate = None
+
+    def be_in_gym(self):
+        print('Estoy en el gimnasio')
+
+class Trainer(Person):
+    salary = None
+
+    def be_in_gym(self):
+        print('estoy trabajando')
+
+class Member(Person):
+    imc = None
+
+    def be_in_gym(self):
+        print('Estoy ejercitandome')
+
+alumno = Member()
+alumno.be_in_gym()
+
+persona = Person()
+print(type('persona'))
+
+from abc import ABC, abstractmethod
+
+class Vehicle(ABC):
+    doors = None
+    wheels = None
+
+    @abstractmethod
+    def avanzar(self):
+        pass
+
+class Car(Vehicle):
+    doors = 5 
+    wheels = 4
+
+    def avanzar(self):
+        return 'Avanzo en 4 ruedas'
+
+class Bike(Vehicle):
+    doors = 0
+    wheels = 2
+
+    def avanzar(self):
+        print (f'Avanzo en {self.wheels} ruedas')
+
+    def __str__(self) -> str:
+        return f'Esta es una moto con {self.doors} puertas y {self.wheels} ruedas'
+
+    
+#Vehiculo = Vehicle()
+#Vehiculo.avanzar()
+carro = Car()
+print(carro.doors)
+
+bike = Bike()
+bike.avanzar()
+print(bike)
+
+
+class User():
+    password = 'Contraseña-encriptada'
+
+    def __init__(self) -> None:
+        self.password = self.__decrypt()
+
+    def __decrypt(self):
+        return 'Contraseña-desencriptada'
+
+usuario = User()
+print(usuario.password)
+
+
