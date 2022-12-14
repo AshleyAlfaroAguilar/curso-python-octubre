585fe0bf0f03f0865542fa0c82f33ccf703cf4ce
Ashley Alfaro <alfaroashley25@outlook.es>
2022-12-14 11:17:11 -0600

    add examples using requests library

---
+++ api_requests.py
@@ -0,0 +1,15 @@
+import requests
+
+response = requests.get('https://www.breakingbadapi.com/api/characters',
+ params={'category': 'Better+Call+Saul'},
+ headers={'Accept': 'aplication/json'})
+
+requests.post('url',data={'key': 'value'})
+
+print(response.status_code)
+print(response.json())
+#walter = response.json()[0]
+#print(walter['name'])
+#print(walter['birthday'])
+
+print(response.headers)
