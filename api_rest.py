379a286a32a62058e26a5c86d3051fec173ff2f6
Ashley Alfaro <alfaroashley25@outlook.es>
2022-12-13 21:35:44 -0600

    add API rest examples

---
+++ api_rest.py
@@ -0,0 +1,17 @@
+# URL https://www.facebook.com/publicaciones 
+# Metodo GET - obtener POST - crear PUT - actualizar DELETE - borra
+# Body - JSON
+{
+    "Nombre":"Rafael",
+    "apellido":"Flores",
+    "altura":172
+}
+# Headers Authorizaton: abc-123
+# Path parameters - /publicaciones/1
+# Query params - /publicaciones?owner=5&page=2
+
+#response
+{
+    "message":"Usuario registrado exitosamente"
+}
+# Codigo HTTP
